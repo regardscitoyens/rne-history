7fa833a603fc8599287221a7d16980d329d4f8c3
RCbot <regardscitoyens@gmail.com>
2022-03-27 18:45:55 +0200

    use pyenv

--- run.sh
+++ run.sh
@@ -2,5 +2,14 @@
 
 cd $(dirname $0)
 
-source venv/bin/activate
+#source venv/bin/activate
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"    # if `pyenv` is not already on PATH
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+pyenv activate rne
+
 python3 src/check_rne.py
